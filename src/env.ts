84d018c9578d8f20ae0f4effcce84751d08c865b
gh-johnny <jmfurtadoromero@gmail.com>
2024-05-09 11:03:08 -0300

    refactor: .env types zod removed
    - .env types are not set through zod anymore
    - .env types are now set through type declarations in types/global.d.ts

--- src/env.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod'
-
-const envSchema = z.object({
-    API_URL: z.string().url(),
-})
-
-export const env = envSchema.parse(process.env)
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-import { env } from '@/env'
-
 export const api = axios.create({
-    baseURL: env.API_URL,
+    baseURL: process.env.NEXT_PUBLIC_BRASILAPI_URL
 })
